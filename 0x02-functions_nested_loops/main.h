1b238d22eab269efb78068ea8dcbbf377418d5b4
McOuma <oumamugah@gmail.com>
2023-02-21 23:16:22 +0300

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#ifndef main.h
-#define main.h
-char _putchar(char c);
+#ifndef main_h
+#define main_h
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +13,4 @@ void times_table(void);
 int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif /* main.h */
+#endif
