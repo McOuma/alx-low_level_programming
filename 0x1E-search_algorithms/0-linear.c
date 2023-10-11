a9c39aafd643dde312151dd58b533cf4ded5e54c
McOuma <oumamugah@gmail.com>
2023-10-11 23:14:20 +0300

    betty style correction

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,18 +7,15 @@
  *
  * Return: index where user looks for
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
-
-	if (array == NULL)
-		return (-1);
-
-	for (; i < size; i++)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	} return (-1);
+size_t i = 0;
+if (array == NULL)
+return (-1);
+for (; i < size; i++)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+} return (-1);
 }
